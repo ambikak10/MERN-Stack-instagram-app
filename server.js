4de67e1fa7dda535740417dce34b08fbf0728682
Ambika K <ambikak10@gmail.com>
2020-06-15 20:52:01 -0700

    mongo URI

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express'); 
 const app = express(); 
+const mongoose = require('mongoose');
+
+//db config
+
+const db = require('./config/keys').mongoURI;
+
+
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+//connect to mongodb
+//DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version.To use the new parser, pass option { useNewUrlParser: true } 
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Mongodb connected"))
+  .catch((err) => console.log(err));
 
 //First route
 app.get('/', (req, res) => res.send('Hello'));
