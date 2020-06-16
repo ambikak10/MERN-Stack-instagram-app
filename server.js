a9643702a9aec1d16569b16d5c5acddc240d2c33
Ambika K <ambikak10@gmail.com>
2020-06-15 21:10:33 -0700

    duplicate code removed

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express'); 
-const mongoose = require('mongoose');
 const app = express(); 
 const mongoose = require('mongoose');
 
@@ -17,15 +16,6 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Mongodb connected"))
   .catch((err) => console.log(err));
-
-//db config
-const db= require('./config/keys').mongoURI;
-
-//connect to mongodb
-//DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version.To use the new parser, pass option { useNewUrlParser: true } 
-mongoose.connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Mongodb connected"))
-  .catch(err => console.log(err));
 
 //First route
 app.get('/', (req, res) => res.send('Hello'));
