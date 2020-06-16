9c6cd9293393173c3de211f8f53507e0628d1379
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-06-16 16:33:25 -0700

    login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,4 +44,30 @@ router.post("/signup", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+// @route   POST api/users/login
+// @desc    Login user
+// @access  Public
+
+router.post('/login', (req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  User.findOne({email})
+    .then(user => {
+      if(!user){
+        return res.status(404).json({email: 'User not found'});
+      }
+      //check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            return res.json({msg: 'Success'});
+          }else{
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        })      
+    })
+    .catch();  
+})
+
 module.exports = router;
