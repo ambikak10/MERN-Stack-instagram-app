1f461aaf2741f91e7f8f5191ea74ef05ad0cbfbc
Ambika K <ambikak10@gmail.com>
2020-06-15 17:32:44 -0700

    instadb

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
  mongoURI : 
-    'mongodb+ srv://team_2:fzjEkoEcuw5Wsa44@instagram-app-n5las.mongodb.net/<instadb>?retryWrites=true&w=majority'
+    'mongodb+ srv://team_2:fzjEkoEcuw5Wsa44@instagram-app-n5las.mongodb.net/instadb?retryWrites=true&w=majority'
  
 }
