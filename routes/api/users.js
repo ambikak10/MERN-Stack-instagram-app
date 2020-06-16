89d7fa84539d2ba7039da69e7387157ee87ff9a9
Ambika K <ambikak10@gmail.com>
2020-06-16 13:45:12 -0700

    password hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,37 +1,49 @@
-const express = require('express');
-const gravatar = require('gravatar');
-
+const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   POST api/users/signup
-// @desc    Sign up new user 
+// @desc    Register user
 // @access  Public
-router.post('/signup', (req, res) => {
-  User.findOne({email: req.body.email})
-    .then(user => {
+router.post("/signup", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
       if (user) {
-        return res.status(400).json({msg: "Email already existed"});
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: "200",
+          r: "pg",
+          d: "mm",
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
+          });
+        });
       }
-
-      const avatar = gravatar.url(req.body.email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
-      });
-
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        avatar,
-        password: req.body.password
-      });
-
-      newUser.save()
-        .then(user => res.json(user))
-        .catch(err => console.log(err));
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
+
+module.exports = router;
 
 module.exports = router;
