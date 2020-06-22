6395087f21c90f3037e2b05aa79598b64c36a053
Quyen Tran <tranngtuquyen@gmail.com>
2020-06-22 11:41:42 -0700

    Creating profile schema

---
+++ models/Profile.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  bio: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  following: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  followers: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    }
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
