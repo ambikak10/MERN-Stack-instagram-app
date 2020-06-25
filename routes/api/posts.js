68ca81ec95d633e9f88a6e52aae1507efd636f8c
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-06-24 19:01:22 -0700

    get , get by id , delete by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -32,4 +32,54 @@ router.post("/",
   }
 )
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "No post found" })
+        );
+    });
+  }
+);
+
+
+
 module.exports = router;
