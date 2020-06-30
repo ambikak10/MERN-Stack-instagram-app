4abd6757e2c895316ac8b521e026f912c5cdfbbf
Quyen Tran <tranngtuquyen@gmail.com>
2020-06-29 20:00:25 -0700

    Create ConversationSchema and create/get routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const users = require('./routes/api/users');
 const passport = require('passport');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const message = require('./routes/api/message');
 
 //Body parser configuration
 app.use(bodyparser.urlencoded({extended: false}));
@@ -37,6 +38,7 @@ app.get('/', (req, res) => res.send('Hello'));
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+app.use('/api/message', message);
 
 const port = 7000;
 
