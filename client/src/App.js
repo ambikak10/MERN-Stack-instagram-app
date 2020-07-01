39e8f67b9328ba3a21d6d63f2ae7eba401a10b84
Ambika K <ambikak10@gmail.com>
2020-06-30 22:52:26 -0700

    login component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
 
 class App extends Component {
   render() {
@@ -9,7 +10,7 @@ class App extends Component {
     return (
     <Router>
       <div className="App">
-        {/* <Route exact path="/signup" component={Signup} /> */}
+        <Route exact path="/signup" component={Signup} />
         <Route exact path="/" component={Login} />
       </div>
     </Router>
