c699691e363b9f5614a18c3b93157d63535dc40b
Ambika K <ambikak10@gmail.com>
2020-07-02 11:57:53 -0700

    profile component

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,20 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
+import Profile from "./components/profile/Profile";
 
 class App extends Component {
   render() {
    
     return (
-    <Router>
-      <div className="App">
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/" component={Login} />
-      </div>
-    </Router>
+      <Router>
+        <div className='App'>
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/' component={Login} />
+          <Route exact path='/profile' component={Profile} />
+   
+        </div>
+      </Router>
     );
   }
 }
