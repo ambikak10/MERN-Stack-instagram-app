2f2952bcab7c5c9c99dc5b22f9190a8b0f27446f
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-07-02 17:02:40 -0700

    client reinstall

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/Signup";
-import Profile from "./components/Layout/profile/Profile";
 
-class App extends Component {
-  render() {
-   
-    return (
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <Route exact path='/signup' component={Signup} />
-          <Route exact path='/' component={Login} />
-          <Route exact path='/profile' component={Profile} />
-   
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
