5e48a8f8a1aaade854f90fa7acc2f2b997f51694
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-07-03 23:11:15 -0700

    signup

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,10 +1,65 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import logo from '../../img/logo.png';
 
 class Signup extends Component {
   render() {
     return (
-      <div>
-        <h1> Good Morning girls! haha </h1>
+      <div className="margin">
+        <div className="d-flex flex-column">
+          <div className="card">
+            <div className="card-body">
+              <img className="logo" src={logo} alt="instagram" />
+               <p className="info">
+                Sign up to see photos and videos                 
+                from your friends.
+              </p>
+              <form action="">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email"
+                    name="email"
+                  />
+                  <input
+                    type="fullname"
+                    className="form-control"
+                    placeholder="Full Name"
+                    name="fullname"
+                  />
+                  <input
+                    type="username"
+                    className="form-control"
+                    placeholder="Username"
+                    name="username"
+                  />
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <input
+                    type="submit"
+                    value="Sign Up"
+                    className="button"
+                  />
+                </div>
+              </form>
+              <p className="terms">
+                By signing up,you agree to our <b>Terms, Data Policy and Cookies Policy.</b>
+              </p>
+             
+              <p className="more-info">
+                 Have an Account? &nbsp;
+                <span>
+                  <Link to="/">Log in</Link>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
