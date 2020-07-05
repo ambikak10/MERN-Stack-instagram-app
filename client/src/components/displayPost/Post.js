b527d9f555f55ca933865b2a094fe326b93488a9
Ambika K <ambikak10@gmail.com>
2020-07-05 16:14:55 -0700

    post component

--- client/src/components/displayPost/Post.js
+++ client/src/components/displayPost/Post.js
@@ -7,46 +7,46 @@ class post extends Component {
   render() {
     return (
      
-        <div className='parent'>
-          <div className='child'>
-            <span class='close'>
+        <div className="parent">
+          <div className="child">
+            <span class="close">
               <button>
-                <i class='fa fa-times' aria-hidden='true'></i>
+                <i class="fa fa-times" aria-hidden="true"></i>
               </button>
             </span>
-            <div className='container-post'>
+            <div className="container-post">
               <img
-                className='size-of-image'
-                // width='600'
-                // height='600'
-                src='https://images.unsplash.com/photo-1462216589242-9e3e00a47a48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=966&q=80'
+                className="size-of-image"
+                // width="600"
+                // height="600"
+                src="https://images.unsplash.com/photo-1462216589242-9e3e00a47a48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=966&q=80"
               />
 
-              <div className='style d-none d-xl-block'>
-                <Link to='#'>
-                  <img className='avatar-icon' src={avatar} alt='Avatar' />
+              <div className="style d-none d-xl-block">
+                <Link to="#">
+                  <img className="avatar-icon" src={avatar} alt="Avatar" />
                 </Link>
-                <Link to='' className='name-of-account'>
+                <Link to="" className="name-of-account">
                   HandleName
                 </Link>
                 <hr style={{ marginBottom: "10px" }} />
 
                 {/*  post description & comments on post */}
-                <div className='comment-wrapper'>
-                  <section className='row'>
+                <div className="comment-wrapper">
+                  <section className="row">
                     {/* <!-- post description start--> */}
 
-                    <div class='col-lg-2'>
-                      <Link to='#'>
-                        <img class='avatar-icon' src={avatar} alt='Avatar' />
+                    <div class="col-lg-2">
+                      <Link to="#">
+                        <img class="avatar-icon" src={avatar} alt="Avatar" />
                       </Link>
                     </div>
-                    <div class='col-lg-10'>
-                      <div id='col-space'>
-                        <Link class='handlename-post' to=''>
+                    <div class="col-lg-10">
+                      <div id="col-space">
+                        <Link class="handlename-post" to="">
                           HandleName
                         </Link>
-                        <span class='textStyle-comment'>
+                        <span class="textStyle-comment">
                           &nbsp; Post description
                         </span>
                       </div>
@@ -54,21 +54,21 @@ class post extends Component {
                     {/* <!-- post description end--> */}
 
                     {/* comments on post */}
-                    <div className='col-lg-2'>
-                      <Link to='#'>
+                    <div className="col-lg-2">
+                      <Link to="#">
                         <img
-                          className='avatar-icon'
+                          className="avatar-icon"
                           src={avatar}
-                          alt='Avatar'
+                          alt="Avatar"
                         />
                       </Link>
                     </div>
-                    <div className='col-lg-8'>
-                      <div id='col-space'>
-                        <Link to='' className='handlename-post'>
+                    <div className="col-lg-8">
+                      <div id="col-space">
+                        <Link to="" className="handlename-post">
                           user1
                         </Link>
-                        <span className='textStyle-comment'>
+                        <span className="textStyle-comment">
                           &nbsp; Lorem ipsum dolor sit amet, consectetur
                           adipiscing elit, sed do eiusmod tempor incididunt ut
                           labore et dolore magna aliqua. Ut enim ad minim
@@ -79,8 +79,8 @@ class post extends Component {
                       </div>
                     </div>
                     {/* <!--delete my comment-rendered conditionally in react--> */}
-                    <div className='col-lg-2'>
-                      <Link to='' className='delete-post'>
+                    <div className="col-lg-2">
+                      <Link to="" className="delete-post">
                         <i
                           style={{
                             fontSize: "0.9em",
@@ -89,27 +89,27 @@ class post extends Component {
                             marginTop: "-3px",
                             fontWeight: "lighter",
                           }}
-                          className='fa fa-trash'
-                          aria-hidden='true'
+                          className="fa fa-trash"
+                          aria-hidden="true"
                         ></i>
                       </Link>
                     </div>
 
-                    <div className='col-lg-2'>
-                      <Link to='#'>
+                    <div className="col-lg-2">
+                      <Link to="#">
                         <img
-                          className='avatar-icon'
+                          className="avatar-icon"
                           src={avatar}
-                          alt='Avatar'
+                          alt="Avatar"
                         />
                       </Link>
                     </div>
-                    <div className='col-lg-10'>
-                      <div id='col-space'>
-                        <Link to='' className='handlename-post'>
+                    <div className="col-lg-10">
+                      <div id="col-space">
+                        <Link to="" className="handlename-post">
                           user2
                         </Link>
-                        <span className='textStyle-comment'>
+                        <span className="textStyle-comment">
                           &nbsp; Lorem ipsum dolor sit amet, consectetur
                           adipiscing elit, sed do eiusmod tempor incididunt ut
                           labore et dolore magna aliqua. Ut enim ad minim
@@ -120,21 +120,21 @@ class post extends Component {
                       </div>
                     </div>
 
-                    <div className='col-lg-2'>
-                      <Link to='#'>
+                    <div className="col-lg-2">
+                      <Link to="#">
                         <img
-                          className='avatar-icon'
+                          className="avatar-icon"
                           src={avatar}
-                          alt='Avatar'
+                          alt="Avatar"
                         />
                       </Link>
                     </div>
-                    <div className='col-lg-10'>
-                      <div id='col-space'>
-                        <Link to='' className='handlename-post'>
+                    <div className="col-lg-10">
+                      <div id="col-space">
+                        <Link to="" className="handlename-post">
                           user3
                         </Link>
-                        <span className='textStyle-comment'>
+                        <span className="textStyle-comment">
                           &nbsp; Lorem ipsum dolor sit amet, consectetur
                           adipiscing elit, sed do eiusmod tempor incididunt ut
                           labore et dolore magna aliqua. Ut enim ad minim
@@ -147,34 +147,34 @@ class post extends Component {
                   </section>
                 </div>
 
-                <div id='footer'>
+                <div id="footer">
                   <hr />
                   <section>
-                    <Link to='' class='delete-post'>
+                    <Link to="" class="delete-post">
                       <i
-                        className='fa fa-heart-o'
+                        className="fa fa-heart-o"
                         style={{ fontSize: "1.5em" }}
-                        aria-hidden='true'
+                        aria-hidden="true"
                       ></i>
                     </Link>
-                    <Link to='' class='delete-post'>
+                    <Link to="" class="delete-post">
                       <i
                         style={{ fontSize: "1.5em" }}
-                        className='fa fa-comment-o'
-                        aria-hidden='true'
+                        className="fa fa-comment-o"
+                        aria-hidden="true"
                       ></i>
                     </Link>
 
-                    <Link to='' class='delete-post'>
+                    <Link to="" class="delete-post">
                       <i
                         style={{ fontSize: "1.5em" }}
-                        className='far fa-user-circle'
-                        aria-hidden='true'
+                        className="far fa-user-circle"
+                        aria-hidden="true"
                       ></i>
                     </Link>
 
                     {/* delete post */}
-                    <Link to='' className='delete-post'>
+                    <Link to="" className="delete-post">
                       <i
                         style={{
                           fontSize: "1.5em",
@@ -183,19 +183,19 @@ class post extends Component {
                           marginTop: "-3px",
                           fontWeight: "lighter",
                         }}
-                        className='fa fa-trash'
-                        aria-hidden='true'
+                        className="fa fa-trash"
+                        aria-hidden="true"
                       ></i>
                     </Link>
 
-                    <p className='textStyle-date'>
+                    <p className="textStyle-date">
                       MARCH 24 &nbsp; <span>21 Likes</span>
                     </p>
                   </section>
                   <hr />
                   <textarea
-                    className='comment-textarea'
-                    type='text'
+                    className="comment-textarea"
+                    type="text"
                     placeholder='Add a comment..'
                   ></textarea>
                   <button className='post'>Post</button>
