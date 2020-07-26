d13936b1ca0f59b1035379c440ff054c0ac872af
Ambika K <ambikak10@gmail.com>
2020-07-26 11:29:45 -0700

    Profile Post display

--- client/src/components/profile/ProfilePostItem.js
+++ client/src/components/profile/ProfilePostItem.js
@@ -8,7 +8,7 @@ class ProfilePostItem extends Component {
       console.log(post);
       return (
         <div>
-          <div className='col-lg-4 col-md-6 col-xs-12 col-xxs-12'>
+          <div className='col-lg-4 col-md-6 col-xs-12 col-xxs-12 responsiveness'>
             <Link to={`/post/${post._id}`}>
               <figure>
                 <img src={post.image} alt='' />
