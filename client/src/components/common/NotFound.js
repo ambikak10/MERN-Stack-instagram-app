d81120479b55db259354eb259e3b78940bd1fdab
Quyen Tran <tranngtuquyen@gmail.com>
2020-07-25 23:23:33 -0700

    Design NotFound page

--- client/src/components/common/NotFound.js
+++ client/src/components/common/NotFound.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import "./notfound.css";
 
 class NotFound extends Component {
   render() {
     return (
-      <div>
-        Page not found
+      <div className="not-found">
+        <div className="not-found-text">Page Not Found</div>
+        <div className="not-found-subtext">Sorry, this page does not exist!</div>
       </div>
     )
   }
