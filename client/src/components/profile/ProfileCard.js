59a7a317bf6bf08686f91028cfddfc0df9920cc1
Ambika K <ambikak10@gmail.com>
2020-07-27 23:08:44 -0700

    Explore component redux

--- client/src/components/profile/ProfileCard.js
+++ client/src/components/profile/ProfileCard.js
@@ -1,43 +1,64 @@
 import React, { Component } from 'react';
 import avatar from '../../img/avatar.png';
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { followUser, unfollowUser } from "../../actions/profileActions";
 
 class ProfileCard extends Component {
   constructor() {
     super();
     this.state = {
-      follow: false
+      follow: false,
     };
-    this.handleFollow = this.handleFollow.bind(this);
-    this.handleUnfollow = this.handleUnfollow.bind(this);
   }
 
-  handleFollow() {
-    this.setState({follow: true});
+  handleFollow(profileId) {
+    this.setState({ follow: true });
+    this.props.followUser(profileId);
   }
 
-  handleUnfollow() {
-    this.setState({follow: false});
+  handleUnfollow(profileId) {
+    this.setState({ follow: false });
+    this.props.unfollowUser(profileId);
   }
-  
   render() {
-    const {profile} = this.props;
+    const { profile } = this.props;
     return (
-      <div className="d-flex card profile-card-outer">
-        <div style={{marginTop: "10px"}}>
-          <Link to={`/profile/${profile.handle}/${profile.user._id}`}><img className="rounded-circle profile-card-avatar" src={profile.user.avatar} alt="avatar"/></Link>
+      <div className='d-flex card profile-card-outer'>
+        <div style={{ marginTop: "10px" }}>
+          <Link to={`/profile/${profile.handle}/${profile.user._id}`}>
+            <img
+              className='rounded-circle profile-card-avatar'
+              src={profile.user.avatar}
+              alt='avatar'
+            />
+          </Link>
         </div>
-        <div className="profile-card-truncate-text">
-          {profile.user.name}
-        </div>
-        <div className="profile-card-card-footer">
-          {!this.state.follow && <button className="btn profile-card-button-follow" onClick={this.handleFollow}>Follow</button>}
-          {this.state.follow && <button className="btn profile-card-button-unfollow" onClick={this.handleUnfollow}>Following</button>}
+        <div className='profile-card-truncate-text'>{profile.user.name}</div>
+        <div className='profile-card-card-footer'>
+          {!this.state.follow && (
+            <button
+              className='btn profile-card-button-follow'
+              onClick={this.handleFollow.bind(this, profile._id)}
+            >
+              Follow
+            </button>
+          )}
+          {this.state.follow && (
+            <button
+              className='btn profile-card-button-unfollow'
+              onClick={this.handleUnfollow.bind(this, profile._id)}
+            >
+              Following
+            </button>
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
-
-export default ProfileCard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { followUser, unfollowUser})(ProfileCard);
 
--- client/src/components/profile/Profiles.js
+++ client/src/components/profile/Profiles.js
@@ -4,17 +4,20 @@ import ProfileCard from './ProfileCard';
 import{getAllProfiles} from '../../actions/profileActions';
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
-
+import Spinner from '../common/Spinner';
 class Profiles extends Component {
   componentDidMount() {
     this.props.getAllProfiles();
   }
   render() {
-    // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-    const {profiles} = this.props.profile;
+    let content;
+    const {profiles, loading} = this.props.profile;
     console.log(this.props.profile.profiles);
-    let content = profiles.map(profile => <ProfileCard profile={profile} key={profile._id}/>);
-
+    if(profiles === null || loading === true){
+    content = <Spinner />;
+    } else {
+      content = profiles.map(profile => <ProfileCard profile={profile} key={profile._id}/>);
+    }
     return (
       <div className='container d-flex flex-wrap' style={{ marginTop: "30px" }}>
         {content}
