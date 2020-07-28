d4955d3c92b03c097e587b3729b0d0177d0d1893
Ambika K <ambikak10@gmail.com>
2020-07-28 14:50:47 -0700

    rremoved console.logs

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,7 +52,7 @@ router.post(
 // @desc    Get  all posts
 // @access  Public
 router.get("/", (req, res) => {
-  console.log("allpost");
+ 
   Post.find()
     .sort({ date: -1 })
     .then((posts) => res.json(posts))
@@ -65,17 +65,17 @@ router.get("/", (req, res) => {
 router.get("/selected",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    console.log('selected');
+  
     Post.find()
       .sort({date: -1})
       .then(posts => {
         if (posts) {
-          console.log(posts);
+       
           let selected = posts.filter(post => {
-            // console.log(post);
+         
             post.user.toString() !== req.user.id
           });
-          console.log(post.user)
+        
           return res.json(selected);
         } else {
           return res.status(404).json({ nopostsfound: "No posts found" });
@@ -97,7 +97,7 @@ router.get(
     Post.find({ user: req.user.id })
       .sort({ date: -1 })
       .then((posts) => {
-        console.log("allpostuser");
+       
         return res.json(posts);
       })
       .catch((err) => res.status(404).json({ nopostsfound: "No posts found" }));
@@ -258,12 +258,12 @@ router.post(
           image: post.image,
           postId: req.params.post_id
         }; 
-      //  console.log(post.image);
+     
         // othersProfile.tagged.unshift({ postId: req.params.post_id });
         othersProfile.tagged.unshift(taggedPost);
         othersProfile.save().then(() => {
            res.json({ msg: 'success' });
-          // console.log(othersProfile);
+       
         });
       });
     }).catch((err) =>
