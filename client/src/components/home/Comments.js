8072254977db79c785929d5371e2b85afc0d2dd4
Ambika K <ambikak10@gmail.com>
2020-07-30 14:13:26 -0700

    if condition in comments.js added

--- client/src/components/home/Comments.js
+++ client/src/components/home/Comments.js
@@ -15,25 +15,38 @@ class Comments extends Component {
 //       viewAllComments: !this.state.viewAllComments,
 //     });
 //   };
+
+
+//   render() {
+//     let comments;
+//     if (this.props.comments) {
+//       comments = this.props.comments.map(comment => (
+//         <CommentItem key={comment.id} comment={comment} postId={this.props.postId}
+//          showAvatar={this.props.showAvatar}
+//           showDelete={this.props.showDelete}/>
+//       ));
+//     }
+    
+//     return (
+//       <div>
+//         {comments}
+//       </div>
+//     )
+//   }
+// }
        
   render() {
     const {postId} = this.props;
     let filterComments =  this.props.comments.filter((comment, index) => index <=2) //filtering to get only first 3 comments
     // let comments;
     let first3comments;
+    if(this.props.comments) {
      first3comments = filterComments.map(comment => (
         <CommentItem key={comment.id} comment={comment} postId={this.props.postId}
          showAvatar={this.props.showAvatar}
           showDelete={this.props.showDelete} s/>
       ));
- 
-      //  comments = this.props.comments.map(comment => (
-      //   <CommentItem key={comment.id} comment={comment} postId={this.props.postId}
-      //    showAvatar={this.props.showAvatar}
-      //     showDelete={this.props.showDelete}/>
-      // ));
-    
-    
+     }
     return (
       <div>
          {this.props.comments.length > 3 &&  (<Link
