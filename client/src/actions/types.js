0db7bd00f1c02e8e58d102d1a3f9a988322ee7ac
Quyen Tran <tranngtuquyen@gmail.com>
2020-07-31 23:36:00 -0700

    Fix ClearProfile

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,6 +14,7 @@ export const CLEAR_POST = "CLEAR_POST";
 export const GET_FOLLOWING = "GET_FOLLOWING";
 export const GET_ALL_PROFILES = "GET_ALL_PROFILES";
 export const GET_CURRENT_PROFILE = "GET_CURRENT_PROFILE";
+export const CLEAR_PROFILE = "CLEAR_PROFILE";
 
 
 
