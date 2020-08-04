3cb41a231ce65eac1b8967ca81d10495bde7f58e
Ambika K <ambikak10@gmail.com>
2020-08-04 11:55:47 -0700

    removed redux devtools extension from store.js

--- client/src/store.js
+++ client/src/store.js
@@ -5,6 +5,11 @@ import rootReducer from './reducers';
 const middleware = [thunk];
 const initialState = {};
 
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+// const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
 
 export default store;
